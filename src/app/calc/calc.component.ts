d9b673743e562f8000811e6f29cfae2d9aafb664
unknown <sud.98.ss@gmail.com>
2023-03-12 23:51:22 +0530

    added calc component

---
+++ src/app/calc/calc.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calc',
+  templateUrl: './calc.component.html',
+  styleUrls: ['./calc.component.css']
+})
+export class CalcComponent {
+  ngOnInit(){
+    console.log("calculator works")
+  }
+}
