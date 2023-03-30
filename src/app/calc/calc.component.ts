495d3e8a1c9190919c7226bb9e7575e8a330d578
sud-98 <63258799+sud-98@users.noreply.github.com>
2023-03-30 23:50:02 +0530

    added background and operator press logic

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -9,6 +9,11 @@ import { FormControl } from '@angular/forms';
 export class CalcComponent {
 
   inputDisplay = new FormControl();
+  operatorClicked : boolean;
+  lastOperator : string;
+
+
+  constructor(){}
 
   ngOnInit(){
     this.inputDisplay.setValue("");
@@ -16,18 +21,79 @@ export class CalcComponent {
 
   clearAll(){
     this.inputDisplay.setValue("");
+    this.operatorClicked = false;
+    this.lastOperator = "";
   }
 
   operatorClick(operator : string){
-    switch (operator){
-      case '=':
-        console.log(this.inputDisplay.value);
+    const val = this.inputDisplay.value;
+    if(!this.operatorClicked && val){
+      this.operatorClicked = true;
+      switch (operator){
+        case '/':
+          this.lastOperator = "/";
+          this.inputDisplay.setValue(val+"/");
+          break
+        case '+':
+          this.lastOperator = "+";
+          this.inputDisplay.setValue(val+"+");
+          break
+          case '-':
+            this.lastOperator = "-";
+            this.inputDisplay.setValue(val+"-");
+            break
+          case '*': 
+            this.lastOperator = "*";
+            this.inputDisplay.setValue(val+"*");
+            break
+      }
     }
+    else{
+      return;
+    }
+    
+    
   }
 
-  numClick(){}
+  numClick(num : string){
+    const val = this.inputDisplay.value;
+    if(num==="0" || num==="00"){
+      if(val===""){
+        return;
+      }
+    }
 
-  del(){}
+    if(num==="."){
+      if(val === ""){
+        this.inputDisplay.setValue("0.");
+      }
+      if(val.includes(".")){
+        return;
+      }
+    }
 
-  
+    this.inputDisplay.setValue(val+num);
+    
+  }
+
+  del(){
+    
+    if(this.inputDisplay.value!=""){
+      const op = this.getLastOperand();
+    if(op==='operator'){
+      this.operatorClicked = false;
+    }
+      this.inputDisplay.setValue(this.inputDisplay.value.substr(0,this.inputDisplay.value.length-1));
+    }
+  }
+  getLastOperand():string {
+    const val = this.inputDisplay.value;
+    const lastOperand = val[val.length-1];
+    if(lastOperand === "+" || lastOperand === "-" || lastOperand === "/" || lastOperand === "*"){
+      return "operator";
+    }
+    return "num";
+  }  
 }
+
+
